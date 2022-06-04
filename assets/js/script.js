595bc02666c7561985cf560289357d080d2fa6b5
Derrick James <ballyouth@gmail.com>
2022-06-04 14:07:03 -0400

    working on event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 var tod = {};
 var svebtn = $(".saveBtn");
 
+
 function tsk1() { 
     var schedule = $('textarea[name="evnt"]').val();
     console.log(schedule);
 }
+function savetask(event) {
+    var bttn = $(event.target);
+    if () {
 
+    }
+    this.parent("div").on("click", tsk1);
+} 
 svebtn.on("click", tsk1);
 
 
