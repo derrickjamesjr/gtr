a3723af2d12f387b00c6217f617a81fedcb6cf21
Derrick James <ballyouth@gmail.com>
2022-06-04 12:49:28 -0400

    fixed text entry for task box

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+console.log("hello");
+var tod = {};
+var svebtn = $(".saveBtn");
+
+function tsk1() { 
+    var schedule = $('textarea[name="evnt"]').val();
+    console.log(schedule);
+}
+
+svebtn.on("click", tsk1);
+
+
